28da566df71c2c2ae54a61f923a0df7bc3ad812f
Ethan Phan <ethan@cinnamon.is>
2018-09-08 16:11:24 +0700

    Move code to build model to a seperate file

--- main.py
+++ main.py
@@ -2,11 +2,13 @@ import os.path
 import tensorflow as tf
 import helper
 import warnings
-from distutils.version import LooseVersion
 import numpy as np
 import sys
 import cv2
 import scipy
+
+from distutils.version import LooseVersion
+from .model import full_network
 
 # Check TensorFlow Version
 assert LooseVersion(tf.__version__) >= LooseVersion(
@@ -25,155 +27,6 @@ KEEP_PROB = 0.75
 EPOCHS = 100000
 BATCH_SIZE = 64
 LR = 0.00003
-
-
-def kernel_initializer():
-    return tf.contrib.layers.xavier_initializer()
-
-
-def regularizer():
-    return tf.contrib.layers.l2_regularizer(scale=0.01)
-
-
-def coordconv_wraper(input):
-    batch_size_tensor = tf.shape(input)[0]
-    x_dim = tf.shape(input)[1]
-    y_dim = tf.shape(input)[2]
-    xx_ones = tf.ones([batch_size_tensor, x_dim], dtype=tf.int32)
-    xx_ones = tf.expand_dims(xx_ones, -1)
-    xx_range = tf.tile(tf.expand_dims(
-        tf.range(y_dim), 0), [batch_size_tensor, 1])
-    xx_range = tf.expand_dims(xx_range, 1)
-    xx_channel = tf.matmul(xx_ones, xx_range)
-    xx_channel = tf.expand_dims(xx_channel, -1)
-
-    yy_ones = tf.ones([batch_size_tensor, y_dim], dtype=tf.int32)
-    yy_ones = tf.expand_dims(yy_ones, 1)
-    yy_range = tf.tile(tf.expand_dims(
-        tf.range(x_dim), 0), [batch_size_tensor, 1])
-    yy_range = tf.expand_dims(yy_range, -1)
-    yy_channel = tf.matmul(yy_range, yy_ones)
-    yy_channel = tf.expand_dims(yy_channel, -1)
-
-    xx_channel = tf.cast(xx_channel, 'float32') / \
-        tf.cast((x_dim - 1), 'float32')
-    yy_channel = tf.cast(yy_channel, 'float32') / \
-        tf.cast((y_dim - 1), 'float32')
-    xx_channel = xx_channel*2 - 1
-    yy_channel = yy_channel*2 - 1
-    ret = tf.concat([input, xx_channel, yy_channel], axis=-1)
-
-    return ret
-
-
-def conv2d_layer(inp_tensor, num_kernels, kernel_size, training, name):
-    inp_tensor = coordconv_wraper(inp_tensor)
-    conv = tf.layers.conv2d(
-        inputs=inp_tensor,
-        filters=num_kernels,
-        kernel_size=(kernel_size, kernel_size),
-        strides=(1, 1),
-        padding='same',
-        activation=tf.nn.relu,
-        kernel_initializer=kernel_initializer(),
-        kernel_regularizer=regularizer(),
-        name=name
-    )
-    return conv
-
-
-def residual_layer(input, num_classes):
-    input = coordconv_wraper(input)
-    res = tf.layers.conv2d(
-        inputs=input,
-        filters=num_classes,
-        kernel_size=(1, 1),
-        strides=(1, 1),
-        padding='same',
-        kernel_initializer=kernel_initializer(),
-    )
-    return res
-
-
-def deconv2d_x2_layer(input, num_classes):
-    input = coordconv_wraper(input)
-    deconv = tf.layers.conv2d_transpose(
-        inputs=input,
-        filters=num_classes,
-        kernel_size=(4, 4),
-        strides=(2, 2),
-        padding='same',
-        kernel_initializer=kernel_initializer(),
-    )
-    return deconv
-
-
-def full_network(num_classes, training=True):
-    _input = tf.placeholder(dtype=tf.float32, shape=[
-                            None, 768, 512, 1], name='input_tensor')
-
-    # conv1
-    conv1_1 = conv2d_layer(_input, 8, 3, training, 'conv1_1')  # 768, 512, 32
-    conv1_2 = conv2d_layer(conv1_1, 8, 3, training, 'conv1_2')
-    pool1 = tf.layers.max_pooling2d(
-        conv1_2, (2, 2), (2, 2), padding='same', name='pool1')
-
-    # conv2
-    conv2_1 = conv2d_layer(pool1, 16, 3, training, 'conv2_1')  # 384, 256, 64
-    conv2_2 = conv2d_layer(conv2_1, 16, 3, training, 'conv2_2')
-    pool2 = tf.layers.max_pooling2d(
-        conv2_2, (2, 2), (2, 2), padding='same', name='pool2')
-
-    # conv3
-    conv3_1 = conv2d_layer(pool2, 32, 3, training, 'conv3_1')  # 192, 128, 128
-    conv3_2 = conv2d_layer(conv3_1, 32, 3, training,
-                           'conv3_2')
-    conv3_3 = conv2d_layer(conv3_2, 32, 3, training,
-                           'conv3_3')
-    pool3 = tf.layers.max_pooling2d(
-        conv3_3, (2, 2), (2, 2), padding='same', name='pool3')
-
-    # conv4
-    conv4_1 = conv2d_layer(pool3, 128, 3, training, 'conv4_1')  # 96, 64, 256
-    conv4_2 = conv2d_layer(conv4_1, 128, 3, training, 'conv4_2')
-    conv4_3 = conv2d_layer(conv4_2, 128, 3, training, 'conv4_3')
-    pool4 = tf.layers.max_pooling2d(
-        conv4_3, (2, 2), (2, 2), padding='same', name='pool4')
-
-    # fc5
-    fc5 = conv2d_layer(pool4, 512, 7, training, 'fc5')  # 48, 32, 512
-    drop5 = tf.layers.dropout(fc5, rate=1 - KEEP_PROB,
-                              training=training)  # 48, 32, 512
-
-    # fc6
-    fc6 = conv2d_layer(drop5, 512, 1, training, 'fc6')  # 48, 32, 512
-    drop6 = tf.layers.dropout(fc6, rate=1 - KEEP_PROB,
-                              training=training)  # 48, 32, 512
-
-    # fc7
-    fc7 = residual_layer(drop6, num_classes)  # 48, 32, num_classes
-
-    pool3_res = residual_layer(pool3, num_classes)  # 96, 64, num_classes
-    pool2_res = residual_layer(pool2, num_classes)  # 192, 128, num_classes
-
-    # Deconv layers
-    deconv8 = deconv2d_x2_layer(fc7, num_classes)  # 96, 64, num_classes
-    sum8 = tf.add(deconv8, pool3_res)  # 96, 64, num_classes
-
-    deconv9 = deconv2d_x2_layer(sum8, num_classes)  # 192, 128, num_classes
-    sum9 = tf.add(deconv9, pool2_res)  # 192, 128, num_classes
-
-    sum9 = coordconv_wraper(sum9)
-    out = tf.layers.conv2d_transpose(
-        inputs=sum9,
-        filters=num_classes,
-        kernel_size=(8, 8),
-        strides=(4, 4),
-        padding='same',
-        kernel_initializer=kernel_initializer(),
-        kernel_regularizer=regularizer())  # 768, 512, num_classes
-
-    return out, _input
 
 
 def optimize(nn_last_layer, correct_label, learning_rate, num_classes):
---
+++ model.py
@@ -0,0 +1,199 @@
+import os.path
+import tensorflow as tf
+import helper
+import warnings
+from distutils.version import LooseVersion
+import numpy as np
+import sys
+import cv2
+import scipy
+
+# Check TensorFlow Version
+assert LooseVersion(tf.__version__) >= LooseVersion(
+    '1.0'), 'Please use TensorFlow version 1.0 or newer.  You are using {}'.format(tf.__version__)
+print('TensorFlow Version: {}'.format(tf.__version__))
+
+# Check for a GPU
+if not tf.test.gpu_device_name():
+    warnings.warn(
+        'No GPU found. Please use a GPU to train your neural network.')
+else:
+    print('Default GPU Device: {}'.format(tf.test.gpu_device_name()))
+
+
+KEEP_PROB = 0.75
+EPOCHS = 100000
+BATCH_SIZE = 64
+LR = 0.00003
+
+
+def kernel_initializer():
+    return tf.contrib.layers.xavier_initializer()
+
+
+def regularizer():
+    return tf.contrib.layers.l2_regularizer(scale=0.01)
+
+
+def coordconv_wraper(input):
+    batch_size_tensor = tf.shape(input)[0]
+    x_dim = tf.shape(input)[1]
+    y_dim = tf.shape(input)[2]
+    xx_ones = tf.ones([batch_size_tensor, x_dim], dtype=tf.int32)
+    xx_ones = tf.expand_dims(xx_ones, -1)
+    xx_range = tf.tile(tf.expand_dims(
+        tf.range(y_dim), 0), [batch_size_tensor, 1])
+    xx_range = tf.expand_dims(xx_range, 1)
+    xx_channel = tf.matmul(xx_ones, xx_range)
+    xx_channel = tf.expand_dims(xx_channel, -1)
+
+    yy_ones = tf.ones([batch_size_tensor, y_dim], dtype=tf.int32)
+    yy_ones = tf.expand_dims(yy_ones, 1)
+    yy_range = tf.tile(tf.expand_dims(
+        tf.range(x_dim), 0), [batch_size_tensor, 1])
+    yy_range = tf.expand_dims(yy_range, -1)
+    yy_channel = tf.matmul(yy_range, yy_ones)
+    yy_channel = tf.expand_dims(yy_channel, -1)
+
+    xx_channel = tf.cast(xx_channel, 'float32') / \
+        tf.cast((x_dim - 1), 'float32')
+    yy_channel = tf.cast(yy_channel, 'float32') / \
+        tf.cast((y_dim - 1), 'float32')
+    xx_channel = xx_channel*2 - 1
+    yy_channel = yy_channel*2 - 1
+    ret = tf.concat([input, xx_channel, yy_channel], axis=-1)
+
+    return ret
+
+
+def conv2d_layer(inp_tensor, num_kernels, kernel_size, training, name):
+    inp_tensor = coordconv_wraper(inp_tensor)
+    conv = tf.layers.conv2d(
+        inputs=inp_tensor,
+        filters=num_kernels,
+        kernel_size=(kernel_size, kernel_size),
+        strides=(1, 1),
+        padding='same',
+        activation=tf.nn.relu,
+        kernel_initializer=kernel_initializer(),
+        kernel_regularizer=regularizer(),
+        name=name
+    )
+    return conv
+
+
+def rcl(X):
+    with tf.variable_scope("rcl") as scope:
+        wr = tf.get_variable('weight_r', [3, 3, self.K, self.K],
+                             initializer=kernel_initializer())
+
+        conv1 = tf.layers.conv2d(X, self.K, kernel_size=(
+            3, 3), padding='same', reuse=None, name='rcl')
+        rcl1 = tf.add(conv1, X)
+        bn1 = tf.contrib.layers.batch_norm(rcl1)
+        #
+        conv2 = tf.layers.conv2d(bn1, self.K, kernel_size=(
+            3, 3), padding='same', reuse=True, name='rcl')
+        rcl2 = tf.add(conv2, X)
+        bn2 = tf.contrib.layers.batch_norm(rcl2)
+        #
+        conv3 = tf.layers.conv2d(bn2, self.K, kernel_size=(
+            3, 3), padding='same', reuse=True, name='rcl')
+        rcl3 = tf.add(conv3, X)
+        bn3 = tf.contrib.layers.batch_norm(rcl3)
+
+        return bn3
+
+
+def residual_layer(input, num_classes):
+    input = coordconv_wraper(input)
+    res = tf.layers.conv2d(
+        inputs=input,
+        filters=num_classes,
+        kernel_size=(1, 1),
+        strides=(1, 1),
+        padding='same',
+        kernel_initializer=kernel_initializer(),
+    )
+    return res
+
+
+def deconv2d_x2_layer(input, num_classes):
+    input = coordconv_wraper(input)
+    deconv = tf.layers.conv2d_transpose(
+        inputs=input,
+        filters=num_classes,
+        kernel_size=(4, 4),
+        strides=(2, 2),
+        padding='same',
+        kernel_initializer=kernel_initializer(),
+    )
+    return deconv
+
+
+def full_network(num_classes, training=True):
+    _input = tf.placeholder(dtype=tf.float32, shape=[
+                            None, 768, 512, 1], name='input_tensor')
+
+    # conv1
+    conv1_1 = conv2d_layer(_input, 8, 3, training, 'conv1_1')  # 768, 512, 32
+    conv1_2 = conv2d_layer(conv1_1, 8, 3, training, 'conv1_2')
+    pool1 = tf.layers.max_pooling2d(
+        conv1_2, (2, 2), (2, 2), padding='same', name='pool1')
+
+    # conv2
+    conv2_1 = conv2d_layer(pool1, 16, 3, training, 'conv2_1')  # 384, 256, 64
+    conv2_2 = conv2d_layer(conv2_1, 16, 3, training, 'conv2_2')
+    pool2 = tf.layers.max_pooling2d(
+        conv2_2, (2, 2), (2, 2), padding='same', name='pool2')
+
+    # conv3
+    conv3_1 = conv2d_layer(pool2, 32, 3, training, 'conv3_1')  # 192, 128, 128
+    conv3_2 = conv2d_layer(conv3_1, 32, 3, training,
+                           'conv3_2')
+    conv3_3 = conv2d_layer(conv3_2, 32, 3, training,
+                           'conv3_3')
+    pool3 = tf.layers.max_pooling2d(
+        conv3_3, (2, 2), (2, 2), padding='same', name='pool3')
+
+    # conv4
+    conv4_1 = conv2d_layer(pool3, 128, 3, training, 'conv4_1')  # 96, 64, 256
+    conv4_2 = conv2d_layer(conv4_1, 128, 3, training, 'conv4_2')
+    conv4_3 = conv2d_layer(conv4_2, 128, 3, training, 'conv4_3')
+    pool4 = tf.layers.max_pooling2d(
+        conv4_3, (2, 2), (2, 2), padding='same', name='pool4')
+
+    # fc5
+    fc5 = conv2d_layer(pool4, 512, 7, training, 'fc5')  # 48, 32, 512
+    drop5 = tf.layers.dropout(fc5, rate=1 - KEEP_PROB,
+                              training=training)  # 48, 32, 512
+
+    # fc6
+    fc6 = conv2d_layer(drop5, 512, 1, training, 'fc6')  # 48, 32, 512
+    drop6 = tf.layers.dropout(fc6, rate=1 - KEEP_PROB,
+                              training=training)  # 48, 32, 512
+
+    # fc7
+    fc7 = residual_layer(drop6, num_classes)  # 48, 32, num_classes
+
+    pool3_res = residual_layer(pool3, num_classes)  # 96, 64, num_classes
+    pool2_res = residual_layer(pool2, num_classes)  # 192, 128, num_classes
+
+    # Deconv layers
+    deconv8 = deconv2d_x2_layer(fc7, num_classes)  # 96, 64, num_classes
+    sum8 = tf.add(deconv8, pool3_res)  # 96, 64, num_classes
+
+    deconv9 = deconv2d_x2_layer(sum8, num_classes)  # 192, 128, num_classes
+    sum9 = tf.add(deconv9, pool2_res)  # 192, 128, num_classes
+
+    sum9 = coordconv_wraper(sum9)
+    out = tf.layers.conv2d_transpose(
+        inputs=sum9,
+        filters=num_classes,
+        kernel_size=(8, 8),
+        strides=(4, 4),
+        padding='same',
+        kernel_initializer=kernel_initializer(),
+        kernel_regularizer=regularizer())  # 768, 512, num_classes
+
+    return out, _input
