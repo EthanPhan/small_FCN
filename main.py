b150326c463328547312bf05924e4f8f43f8293a
ethan <longfet53@gmail.com>
2018-09-12 09:41:22 +0700

    ...

--- main.py
+++ main.py
@@ -2,13 +2,11 @@ import os.path
 import tensorflow as tf
 import helper
 import warnings
+from distutils.version import LooseVersion
 import numpy as np
 import sys
 import cv2
 import scipy
-
-from distutils.version import LooseVersion
-from model import full_network
 
 # Check TensorFlow Version
 assert LooseVersion(tf.__version__) >= LooseVersion(
@@ -25,9 +23,166 @@ else:
 
 KEEP_PROB = 0.75
 EPOCHS = 100000
-BATCH_SIZE = 4
-LR = 0.0001
-CLASS_LOSS_WEIGHTS = [0.0949784, 7, 7]
+BATCH_SIZE = 6
+LR = 0.00003
+
+
+def kernel_initializer():
+    return tf.contrib.layers.xavier_initializer()
+
+
+def regularizer():
+    return tf.contrib.layers.l2_regularizer(scale=0.01)
+
+
+def coordconv_wraper(input):
+    batch_size_tensor = tf.shape(input)[0]
+    x_dim = tf.shape(input)[1]
+    y_dim = tf.shape(input)[2]
+    xx_ones = tf.ones([batch_size_tensor, x_dim], dtype=tf.int32)
+    xx_ones = tf.expand_dims(xx_ones, -1)
+    xx_range = tf.tile(tf.expand_dims(
+        tf.range(y_dim), 0), [batch_size_tensor, 1])
+    xx_range = tf.expand_dims(xx_range, 1)
+    xx_channel = tf.matmul(xx_ones, xx_range)
+    xx_channel = tf.expand_dims(xx_channel, -1)
+
+    yy_ones = tf.ones([batch_size_tensor, y_dim], dtype=tf.int32)
+    yy_ones = tf.expand_dims(yy_ones, 1)
+    yy_range = tf.tile(tf.expand_dims(
+        tf.range(x_dim), 0), [batch_size_tensor, 1])
+    yy_range = tf.expand_dims(yy_range, -1)
+    yy_channel = tf.matmul(yy_range, yy_ones)
+    yy_channel = tf.expand_dims(yy_channel, -1)
+
+    xx_channel = tf.cast(xx_channel, 'float32') / \
+        tf.cast((x_dim - 1), 'float32')
+    yy_channel = tf.cast(yy_channel, 'float32') / \
+        tf.cast((y_dim - 1), 'float32')
+    xx_channel = xx_channel*2 - 1
+    yy_channel = yy_channel*2 - 1
+    ret = tf.concat([input, xx_channel, yy_channel], axis=-1)
+
+    return ret
+
+
+def conv2d_layer(inp_tensor, num_kernels, kernel_size, name, reuse=None):
+    inp_tensor = coordconv_wraper(inp_tensor)
+    conv = tf.layers.conv2d(
+        inputs=inp_tensor,
+        filters=num_kernels,
+        kernel_size=(kernel_size, kernel_size),
+        strides=(1, 1),
+        padding='same',
+        activation=tf.nn.relu,
+        kernel_initializer=kernel_initializer(),
+        kernel_regularizer=regularizer(),
+        name=name,
+        reuse=reuse
+    )
+    return conv
+
+
+def rcl(X, num_kernels, kernel_size, scope_name=None):
+    with tf.variable_scope(scope_name) as scope:
+        input = conv2d_layer(X, num_kernels, kernel_size, 'input')
+
+        for i in range(2):
+            reuse = (i > 0)
+            conv = conv2d_layer(input, num_kernels, kernel_size,
+                                'rcl', reuse)
+            rcl = tf.add(conv, input)
+            bn = tf.contrib.layers.batch_norm(rcl)
+
+            input = bn
+
+        return bn
+
+
+def residual_layer(input, num_classes):
+    input = coordconv_wraper(input)
+    res = tf.layers.conv2d(
+        inputs=input,
+        filters=num_classes,
+        kernel_size=(1, 1),
+        strides=(1, 1),
+        padding='same',
+        kernel_initializer=kernel_initializer(),
+    )
+    return res
+
+
+def deconv2d_x2_layer(input, num_classes):
+    input = coordconv_wraper(input)
+    deconv = tf.layers.conv2d_transpose(
+        inputs=input,
+        filters=num_classes,
+        kernel_size=(4, 4),
+        strides=(2, 2),
+        padding='same',
+        kernel_initializer=kernel_initializer(),
+    )
+    return deconv
+
+
+def full_network(num_classes, training=True):
+    _input = tf.placeholder(dtype=tf.float32, shape=[
+                            None, 768, 512, 1], name='input_tensor')
+
+    # conv1
+    rcl1 = rcl(_input, 8, 3, 'rcl1')  # 768, 512, 32
+    pool1 = tf.layers.max_pooling2d(
+        rcl1, (2, 2), (2, 2), padding='same', name='pool1')
+
+    # conv2
+    rcl2 = rcl(pool1, 16, 3, 'rcl2')  # 384, 256, 64
+    pool2 = tf.layers.max_pooling2d(
+        rcl2, (2, 2), (2, 2), padding='same', name='pool2')
+
+    # conv3
+    rcl3 = rcl(pool2, 32, 3, 'rcl3')  # 192, 128, 128
+    pool3 = tf.layers.max_pooling2d(
+        rcl3, (2, 2), (2, 2), padding='same', name='pool3')
+
+    # conv4
+    rcl4 = rcl(pool3, 64, 3, 'rcl4')  # 96, 64, 256
+    pool4 = tf.layers.max_pooling2d(
+        rcl4, (2, 2), (2, 2), padding='same', name='pool4')
+
+    # fc5
+    fc5 = conv2d_layer(pool4, 512, 7, 'fc5')  # 48, 32, 512
+    drop5 = tf.layers.dropout(fc5, rate=1 - KEEP_PROB,
+                              training=training)  # 48, 32, 512
+
+    # fc6
+    fc6 = conv2d_layer(drop5, 512, 1, 'fc6')  # 48, 32, 512
+    drop6 = tf.layers.dropout(fc6, rate=1 - KEEP_PROB,
+                              training=training)  # 48, 32, 512
+
+    # fc7
+    fc7 = residual_layer(drop6, num_classes)  # 48, 32, num_classes
+
+    pool3_res = residual_layer(pool3, num_classes)  # 96, 64, num_classes
+    pool2_res = residual_layer(pool2, num_classes)  # 192, 128, num_classes
+
+    # Deconv layers
+    deconv8 = deconv2d_x2_layer(fc7, num_classes)  # 96, 64, num_classes
+    sum8 = tf.add(deconv8, pool3_res)  # 96, 64, num_classes
+
+    deconv9 = deconv2d_x2_layer(sum8, num_classes)  # 192, 128, num_classes
+    sum9 = tf.add(deconv9, pool2_res)  # 192, 128, num_classes
+
+    sum9 = coordconv_wraper(sum9)
+    out = tf.layers.conv2d_transpose(
+        inputs=sum9,
+        filters=num_classes,
+        kernel_size=(8, 8),
+        strides=(4, 4),
+        padding='same',
+        kernel_initializer=kernel_initializer(),
+        kernel_regularizer=regularizer())  # 768, 512, num_classes
+
+    return out, _input
 
 
 def optimize(nn_last_layer, correct_label, learning_rate, num_classes):
@@ -50,10 +205,9 @@ def optimize(nn_last_layer, correct_label, learning_rate, num_classes):
     # Because the data is imbalance (the number of pixel with label
     # '0' is many time more than the number of pixel with label '1'
     # use pos weight to avoid lazy learning - always predict '0' )
-    classes_weights = tf.constant(CLASS_LOSS_WEIGHTS)
+    classes_weights = tf.constant([0.0949784, 4, 4])
     loss = tf.reduce_mean(tf.nn.weighted_cross_entropy_with_logits(
-        targets=tf.cast(labels, tf.float32), logits=logits,
-        pos_weight=classes_weights))
+        targets=tf.cast(labels, tf.float32), logits=logits, pos_weight=classes_weights))
 
     train_op = tf.train.AdamOptimizer(
         learning_rate).minimize(loss, global_step=gl_step)
@@ -64,9 +218,8 @@ def optimize(nn_last_layer, correct_label, learning_rate, num_classes):
 # tests.test_optimize(optimize)
 
 
-def run_train(sess, training_steps, batch_size, get_batches_fn, train_op,
-              cross_entropy_loss, input_image, correct_label,
-              learning_rate, saver, gl_step):
+def run_train(sess, training_steps, batch_size, get_batches_fn, train_op, cross_entropy_loss, input_image,
+              correct_label, learning_rate, saver, gl_step):
     """
     Train neural network and print out the loss during training.
     :param sess: TF Session
@@ -128,6 +281,8 @@ def train():
     num_classes = 3
     image_shape = (768, 512)
     data_dir = './data'
+    runs_dir = './runs'
+    run_label = 'E%04d-B%04d-K%f_' % (EPOCHS, BATCH_SIZE, KEEP_PROB)
     # tests.test_for_kitti_dataset(data_dir)
 
     # Download pretrained vgg model
@@ -151,6 +306,8 @@ def train():
         learning_rate = tf.placeholder(tf.float32)
 
         # Build NN using load_vgg, layers, and optimize function
+        # _input, keep_prob, l3_out, l4_out, l7_out = load_vgg(sess, vgg_path)
+        # last_layer = layers(l3_out, l4_out, l7_out, num_classes)
         last_layer, _input = full_network(num_classes)
         logits, train_op, cross_entropy_loss, gl_step = optimize(
             last_layer, correct_label, learning_rate, num_classes)
