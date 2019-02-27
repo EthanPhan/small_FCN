684680fdf2f6f4f13d4436b6c8c043f89bb48d4f
Ethan Phan <ethan@cinnamon.is>
2019-02-27 22:34:15 +0700

    ...

--- model.py
+++ model.py
@@ -188,31 +188,32 @@ def full_network(num_classes, training=True):
     pool4 = max_pooling2d(
         rcl4, (2, 2), (2, 2), padding='same', name='pool4')
 
-    drop6 = tf.layers.dropout(pool4, rate=1 - KEEP_PROB,
-                              training=training)  # 48, 32, 512
+    drop4 = tf.layers.dropout(pool4, rate=1 - KEEP_PROB,
+                              training=training)  # 48, 32, 64
 
-    # fc7
-    fc7 = residual_layer(drop6, num_classes)  # 48, 32, num_classes
+    fc5 = conv2d_layer(drop4, 128, 3, 'fc5')
 
-    pool3_res = residual_layer(pool3, num_classes)  # 96, 64, num_classes
-    pool2_res = residual_layer(pool2, num_classes)  # 192, 128, num_classes
+    # # start up path here
+    up5 = deconv2d_x2_layer(fc5, 64)  # 96, 64, 64
+    up5 = tf.concatenate([rcl4, up5], -1)  # 96, 64, 128
+    up5 = conv2d_layer(up5, 64, 3, 'up5')
+    up5 = attention(up5, 64, 'a4')
 
-    # Deconv layers
-    deconv8 = deconv2d_x2_layer(fc7, 64)  # 96, 64, num_classes
-    deconv8 = attention(deconv8, 64, 'a4')
-    sum8 = tf.add(deconv8, pool3_res)  # 96, 64, num_classes
+    up6 = deconv2d_x2_layer(up5, 32)  # 192, 128, 32
+    up6 = tf.concatenate([rcl3, up6], -1)  # 192, 128, 64
+    up6 = conv2d_layer(up5, 32, 3, 'up6')
 
-    deconv9 = deconv2d_x2_layer(sum8, num_classes)  # 192, 128, num_classes
-    sum9 = tf.add(deconv9, pool2_res)  # 192, 128, num_classes
-
-    sum9 = coordconv_wraper(sum9)
-    out = conv2d_transpose(
-        inputs=sum9,
-        filters=num_classes,
+    wrap7 = coordconv_wraper(up6)
+    up7 = conv2d_transpose(
+        inputs=wrap7,
+        filters=8,
         kernel_size=(8, 8),
         strides=(4, 4),
         padding='same',
         kernel_initializer=kernel_initializer(),
-        kernel_regularizer=regularizer())  # 768, 512, num_classes
+        kernel_regularizer=regularizer())  # 768, 512, 8
+    up7 = tf.concatenate([rcl1, up7], -1)  # 768, 512, 16
+    up7 = conv2d_layer(up7, 8, 3, 'up7')
 
+    out = conv2d_layer(up7, num_classes, 3, 'out')
     return out, _input
